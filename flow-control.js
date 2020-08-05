169f42bdf95b2e12636dc25312313e426f1dab93
Patrick <pdunton@gmail.com>
2020-08-05 23:37:52 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -23,11 +23,11 @@ function ternaryTeenager(age) {
   return if (age >= 13 && <= 19) ? console.log("You are a teenager") : console.log("You are not a teenager")
 }
 
-function switchAge(age) {
+function switchAge(age) 
   switch (age) {
     case (age >= 13 && <= 19):
     console.log("You are a teenager");
     break;
     default:
     console.log("You have an age");
-  }}
+  }
