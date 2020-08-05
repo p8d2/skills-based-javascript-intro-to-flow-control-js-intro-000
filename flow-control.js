c59fa6adc295f4c399cf19ace66a1fa763dfd2bc
Patrick <pdunton@gmail.com>
2020-08-05 22:33:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,7 +2,7 @@ function basicTeenager(age) {
    if (age >= 13 && <= 19) {
     console.log("You are a teenager!") {
    }
-   
+
 function teenager(age) {
    if (age >= 13 && <= 19) {
      console.log("You are a teenager!") {
